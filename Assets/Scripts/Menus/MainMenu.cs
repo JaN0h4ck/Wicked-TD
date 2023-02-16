39fb9895fc6cc2cfae92700e887b4f939a72704d
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-02-16 18:38:38 +0100

    Added Main Menu

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    private GameObject m_mainMenu;
+    private CanvasGroup m_mainMenuCanvasGroup;
+    private GameObject m_settingsMenu;
+    private CanvasGroup m_settingsMenuCanvasGroup;
+
+    private void Start() {
+        CanvasGroup[] tempCanvasGroups = GetComponentsInChildren<CanvasGroup>(true);
+        GameObject[] tempGameObjects = new GameObject[tempCanvasGroups.Length];
+
+        for (int i = 0; i < tempCanvasGroups.Length; i++) {
+            tempGameObjects[i] = tempCanvasGroups[i].gameObject;
+
+            if (tempGameObjects[i].name.Contains("Main")) {
+                m_mainMenuCanvasGroup = tempCanvasGroups[i];
+                m_mainMenu = tempGameObjects[i];
+            } else if (tempGameObjects[i].name.Contains("Settings")) {
+                m_settingsMenuCanvasGroup = tempCanvasGroups[i];
+                m_settingsMenu = tempGameObjects[i];
+            }
+        }
+    }
+
+    public void StartGame() {
+        Debug.Log("Starting Game...");
+    }
+
+    public void gotoSettingsMenu() {
+        m_mainMenu.SetActive(false);
+        m_settingsMenu.SetActive(true);
+    }
+
+    public void returntoMainMenu() {
+        m_mainMenu.SetActive(true);
+        m_settingsMenu.SetActive(false);
+    }
+
+    public void ExitGame() {
+        Debug.Log("Exiting Game...");
+        Application.Quit();
+    }
+}
