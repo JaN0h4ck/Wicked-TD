87e3194dcdebea7eb236e38ac7d1d6989a52d9a6
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-02-18 21:17:13 +0100

    Working Spawn System

--- Assets/Scripts/Enemy/Spawner.cs
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
@@ -12,13 +15,45 @@ public class Spawner : MonoBehaviour
     public uint currentDifficulty;
 
     private float _timeDelayBuffer;
-    public void SpawnWave() {
-        uint spawnDifficultyBuffer = currentDifficulty;
-        uint easy = 1;
-        uint medium = 5;
-        uint hard = 9;
-        
+
+    private void Start() {
+        StartCoroutine(SpawnWave());
     }
+
+    public IEnumerator SpawnWave() {
+        uint spawnDifficultyBuffer = currentDifficulty;
+        const uint easy = 1;
+        const uint medium = 5;
+        const uint hard = 9;
+
+        while(spawnDifficultyBuffer > 0) {
+            if (spawnDifficultyBuffer >= hard) {
+                spawnDifficultyBuffer -= h_EnemyDecider(new uint[] { easy, medium, hard });
+            } else if (spawnDifficultyBuffer >= medium) {
+                spawnDifficultyBuffer -= h_EnemyDecider(new uint[] { easy, medium });
+            } else if (spawnDifficultyBuffer > 0){
+                spawnDifficultyBuffer -= h_EnemyDecider(new uint[] { easy });
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    private uint h_EnemyDecider(uint[] usedDifficulties) {
+        switch (usedDifficulties[Random.Range(0, usedDifficulties.Length - 1)]) {
+            case 1u:
+                GameObject EasyEnemy = Instantiate(easyEnemies[Random.Range(0, easyEnemies.Length - 1)].gameObject, this.transform);
+                return 1u;
+            case 5u:
+                GameObject MediumEnemy = Instantiate(mediumEnemies[Random.Range(0, mediumEnemies.Length - 1)].gameObject, this.transform);
+                return 5u;
+            case 9u:
+                GameObject HardEnemy = Instantiate(hardEnemies[Random.Range(0, hardEnemies.Length - 1)].gameObject, this.transform);
+                return 9u;
+            default:
+                return 0u;
+        }
+    }
+
 
     private bool h_SpawnDelay() {
         _timeDelayBuffer += Time.deltaTime;
